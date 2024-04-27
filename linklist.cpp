7d5845a6acaadf6ba59e5771d2c0ea79da65b81d
anobee <HP@DESKTOP-KFAVCD8>
2024-04-27 08:25:10 +0700

    12

--- linklist.cpp
+++ linklist.cpp
@@ -49,7 +49,7 @@ void addNode() {
 
 }
 
-bool searchNode (int nim, Node*current, node*previous) {
+bool searchNode (int nim, Node*current, Node*previous) {
     previous = START;
     current = START;
     while (current != NULL && nim > current -> noMhs)
